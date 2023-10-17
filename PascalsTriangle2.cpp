c03ce1dec8c8d0ec8f5e07243e22ef97827a4ad3
Iuzof <iuzofatov.rm@phystech.edu>
2023-10-17 12:11:17 +0300

    Validate Binary Tree By Nodes

---
+++ PascalsTriangle2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+  std::vector<int> getRow(int rowIndex) {
+    std::vector<std::vector<int>> paTri;
+    std::vector<int> first{1};
+    paTri.insert(first);
+    int i = 1;
+    while (i <= rowIndex){
+      std::vector<int> new_row;
+      for (int j = 1; j < paTri.size(); j++){
+        
+      }
+      paTri.insert()
+    }
+}
+};
+
+int main(){
+  return 0;
+}
